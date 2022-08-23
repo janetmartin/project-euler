eeddeab5f7196a61c08e73945d12817fa08d0d73
J Martin <info@janetmartin.ca>
2022-08-22 22:07:38 -0700

    Solution to problem 3, largest prime factors of a value.

---
+++ 3_largest_prime.py
@@ -0,0 +1,19 @@
+# Question 3: Largest Prime https://projecteuler.net/problem=3
+
+import math
+
+#checking for primality
+def isprime(num):
+    for n in range(2,int(math.sqrt(num))+1):
+        if num%n==0:
+            return False
+    return True
+
+#checking for prime factors
+def largest_factor(value):
+    for i in range (2, value):
+        if isprime(i) == True:
+            if value % i == 0:
+                print(i)
+
+largest_factor(600851475143)
