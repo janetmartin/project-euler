537c18ed05a076deb1788148b1a2e7120d207ef0
J Martin <info@janetmartin.ca>
2022-08-23 17:17:39 -0700

    Solution to problem 4, largest palindrome.

---
+++ 4_largest_palindrome.py
@@ -0,0 +1,25 @@
+# Question 4: Largest Palindrome https://projecteuler.net/problem=4
+
+# function to reverse a string
+
+def reverse(x):
+    x = str(x)
+    return int(x[::-1])
+
+# function to detect largest palindrome
+
+
+def max_palindrome(val1, val2):
+    product1 = 0
+    for a in range(val1, val2 + 1):
+        for b in range(val1, val2 + 1):
+            product2 = a * b
+            if product2 == reverse(product2):
+                if product2 > product1:
+                    product1 = product2
+    return product1
+
+# testcase
+# print(max_palindrome(10, 99))
+
+print(max_palindrome(100, 999))
