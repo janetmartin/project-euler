6b5f9f689ff0f0cc0325a607142be042b470ab77
J Martin <info@janetmartin.ca>
2022-08-26 11:16:15 -0700

    More refined solution

--- 6_sum_square_difference.py
+++ 6_sum_square_difference.py
@@ -1,18 +1,11 @@
 # Question 6: Sum Square Differences https://projecteuler.net/problem=6
-# solve for diff between sum of squares and square of sums
 
-# function for square of sum with divisor
-def square_of_sums(start, end, divisor):
-    return sum(range(start, end + 1, divisor))**2
+# function for diff betwen square of sums and sum of squares
+def sum_of_squares(end):
+    squares = 0
+    for i in range(0, end + 1):
+        squares += i**2
+    return sum(range(end + 1))**2 - squares
 
-
-# function for sum of squares
-def sum_of_squares(start, end, divisor):
-    squares = []
-    for i in range(start, end + 1, divisor):
-        squares.append(i ** 2)
-    return sum(squares)
-
-
-print("Test Case:", square_of_sums(1, 10, 1) - sum_of_squares(1, 10, 1))
-print("Solution:", square_of_sums(1, 100, 1) - sum_of_squares(1, 100, 1))
+print("Test Case:", sum_of_squares(10))
+print("Solution:", sum_of_squares(100))
