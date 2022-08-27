f03e816011feebeb183464c972e68e4075e53845
J Martin <info@janetmartin.ca>
2022-08-27 15:00:42 -0700

    Solution to question 7, optimized.

--- 7_10001st_prime.py
+++ 7_10001st_prime.py
@@ -21,6 +21,21 @@ def find_prime(nth):
         num += 1
     return primes[-1]
 
+# This 2nd solution iterates over the candidate but does not slow down by appending a list.
 
+def faster_primes(limit):
+    count = 1 # we know that 2 is prime
+    candidate = 2
+    while count < limit:
+        candidate = candidate + 1
+        if isprime(candidate):
+            count=count+1
+        if count == limit:
+            return(candidate)
+
+print("My Solution")
 print("Test case: ", find_prime(6))
 print("Solution: ", find_prime(10001))
+
+print("Faster test case:", faster_primes(6))
+print("Faster solution:", faster_primes(10001))
