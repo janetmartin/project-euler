4c8525235b55ea81f4b87ef1dcf2e35bc2d065a0
J Martin <info@janetmartin.ca>
2023-03-16 14:05:30 -0700

    Create 10_Summation_of_Primes.py
    Solution to question 10, the sum of primes between 1 and 2 million.

---
+++ 10_Summation_of_Primes.py
@@ -0,0 +1,59 @@
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+#
+# Find the sum of all the primes below two million.
+
+# Method:
+# 1. This is slow but will do it without using any modules (optimized solution would use numpy)
+# 2. 1 to 2 million
+# 3. On each loop test the value using the Use the Sieve of Eratosthenes algorithm to see
+# 4. If true add that value to the sum of primes.
+
+# # Solution 1: Seive
+def sieve(n):
+
+    # Define variabes
+    # - the first list to hold calculated multiples
+    # - the second to hold the sieved out prime numbers.
+
+    multiples = []
+    primes = []
+
+
+
+    for i in range(2, n+1):
+        if i not in multiples: # the prime test, if a multiple of n is not in the generated list, then n is prime (only being divisable by itself and one).
+            primes.append(i)
+            for j in range(i*i, n+1, i):
+                multiples.append(j)
+    print(sum(primes))
+
+
+
+
+# Solution 2: A faster sieve
+# optimized solution from https://algodaily.com/challenges/sum-all-primes/python
+
+def sumOfPrimes(n):
+    if n <= 1:
+        return 1 # not considered a prime
+
+    prime = [True] * (n + 1)  # list to store if number is prime number
+
+    p = 2 # start at 2
+    while p * p <= n: # continue loop until upper bound
+        # if p is not changed in prime, then it is prime number
+        if prime[p] == True:
+            i = p * 2  # update all multiples of p
+
+            while i <= n:
+                prime[i] = False
+                i += p
+        p += 1
+
+    totalSum = 0
+    for i in range(2, n + 1):  # find the sum by looking into prime list
+        if prime[i]:
+            totalSum += i
+    return totalSum
+
+sumOfPrimes(2000000))
